4de86110d5b97cbc82026c8caf887ecdd594be87
Stjepan Ruklić <sr51641@fer.hr>
2021-07-06 19:43:04 +0200

    add project

---
+++ battleships/src/main/java/agency04/battleships/BattleshipsApplication.java
@@ -0,0 +1,14 @@
+package agency04.battleships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipsApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(BattleshipsApplication.class, args);
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ battleships/src/test/java/agency04/battleships/BattleshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package agency04.battleships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
